b311b4b8202df419d90a34a135839672c30980f2
Maksim Pavlov <maxim.paw2010@yandex.ru>
2024-08-05 23:57:21 +0300

    btn add

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Product = ({ product }) => {
+  const [isAdded, setIsAdded] = useState(false)
+  const [quantity, setQuantity] = useState(0)
+
+  const handleAdd = () => {
+
+    setIsAdded(true)
+  }
 
   return (
     <div className='product'>
-      <img className='product-img' src={product.image.desktop} alt={product.category} />
+      <div className='product-img-container'>
+        <img className='product-img' src={product.image.desktop} alt={product.category} />
+        {isAdded 
+          ? (
+            <button className='add-to-cart added'>
+              <span className='decrease'></span>
+              {quantity}
+              <span className='increase'></span>
+            </button>
+            )
+          : (
+            <button className='add-to-cart' onClick={handleAdd}>
+              <span className='cart-img'></span>
+              Add to Cart
+            </button>
+            )
+        }
+      </div>
       <span>{product.category}</span>
       <strong>{product.name}</strong>
       <span>${product.price}</span>
